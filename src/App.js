5ebca8a69d1d68c954c6045e109cf63344f4d221
Deathlyassaulter <107764679+Deathlyassaulter@users.noreply.github.com>
2024-09-23 02:51:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Restaurant from "./component/basics/restaurant";
+import Usestate from "./component/hooks/usestate.js";
+
+const App = () => {
+  return <Restaurant />;
+};
+
+export default App;
